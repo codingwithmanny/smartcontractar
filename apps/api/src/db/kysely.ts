60ec85ad99795b87549e36795e47bbb0bd2246de
codingwithmanny <codingwithmanny@gmail.com>
2023-08-07 22:27:44 +0300

    feat: 🎸 Added support for neon db dialect

--- apps/api/src/db/kysely.ts
+++ apps/api/src/db/kysely.ts
@@ -1,17 +1,25 @@
 // Imports
 // ========================================================
-import fs from 'fs';
+import fs from "fs";
 import { Database } from "./types"; // this is the Database interface we defined earlier
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
+import { NeonDialect } from "kysely-neon";
+import ws from "ws";
 
 // Connection
 // ========================================================
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    connectionString: `${process.env.DATABASE_URL}`,
-  }),
-});
+const dialect =
+  process.env.NODE_ENV === "production"
+    ? new NeonDialect({
+        connectionString: process.env.DATABASE_URL,
+        webSocketConstructor: ws,
+      })
+    : new PostgresDialect({
+        pool: new Pool({
+          connectionString: `${process.env.DATABASE_URL}`,
+        }),
+      });
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely
 // knows your database structure.
